8e706755fe6ce33f61c7a07a996074ce1b7d781f
JHeaon <j3heawon@naver.com>
2023-02-13 02:43:27 +0900

    add gitignore

--- chat/app/consumers.py
+++ chat/app/consumers.py
@@ -1,4 +1,6 @@
+import json
 from channels.generic.websocket import WebsocketConsumer
+
 
 class EchoConsumer(WebsocketConsumer):
  # 웹소켓 클라이언트에서
@@ -8,4 +10,8 @@ class EchoConsumer(WebsocketConsumer):
  # binary data frame 으로 보내면 bytes_data 인자에 값이 담겨져 호출됩니다.
  # - ref: https: /ko.javascript.info/websocket#ref-159
     def receive(self, text_data=None, bytes_data=None):
-        self.send(f"You said: {text_data}")
+
+        obj = json.loads(text_data)
+        json_string = json.dumps(obj)
+        self.send(json_string)
+        
--- chat/app/models.py
+++ chat/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    
+    class Meta:
+        ordering = ["-id"] # 쿼리셋의 디폴트 정렬
